ef45c0d5e29d8004afcb22a2a1d2cea4e33fdc31
Taher Ahmed <taherahmed@Tahers-MacBook-Air-2019.local>
2025-04-10 01:34:35 +0100

    style: admin dashboard styling

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -25,14 +25,14 @@ export default function StatCard({
   className,
 }: StatCardProps) {
   return (
-    <Card className={`@container/card !bg-white/10 text-white ${className}`}>
+    <Card className={`@container/card border-gray-700 bg-custom-dark shadow-[0_4px_10px_rgba(0,0,0,0.4)] text-white ${className}`}>
       <CardHeader className="relative">
         <CardDescription className="text-white">{title}</CardDescription>
         <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
           {value}
         </CardTitle>
-        <div className="absolute right-4 top-4">
-          <Badge variant="outline" className="flex gap-1 rounded-lg text-xs text-white">
+        <div className="absolute right-4 top-0">
+          <Badge variant="outline" className="flex gap-1 rounded-sm text-xs text-white border-gray-700 py-1.5" style={{backgroundColor: trend.startsWith("+") ? "rgba(11, 156, 49, 0.4)" : "rgba(255, 0, 0, 0.4)"}}>
             <Icon className="size-3" />
             {trend}
           </Badge>
